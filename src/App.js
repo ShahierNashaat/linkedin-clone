151aba0bfa840c569276747a607c4c1ecc542b41
ShahierNashaat <shahiernashaat97@gmail.com>
2022-04-21 20:54:29 +0200

    Create Login page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h2>Let&apos;s build the linkedin clone!</h2>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
